f7f0dbef987d6ac94d9660fa7c592da689487907
Nathan <natethepcspecialist@gmail.com>
2017-08-10 14:55:20 -0400

    update

--- server/routes/web.js
+++ server/routes/web.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const recursive = require('recursive-readdir');
 router.get('/', function(req, res){
     recursive('server/images', function (err, files) {
         res.render('index', {dirs: files});
