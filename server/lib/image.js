f7673ee09c824066da9bc8f5998032736a8d2ede
Nathan <natethepcspecialist@gmail.com>
2017-08-16 17:52:49 -0400

    update

--- server/lib/image.js
+++ server/lib/image.js
@@ -1,71 +1,71 @@
-const allowedUploads = ['image/jpg', 'image/jpeg', 'image/png'];
-const config = require('../config');
-const multer = require('multer');
-const mkdirp = require('mkdirp');
-const path = require('path');
-var rimraf = require('rimraf');
+// const allowedUploads = ['image/jpg', 'image/jpeg', 'image/png'];
+// const config = require('../config');
+// const multer = require('multer');
+// const mkdirp = require('mkdirp');
+// const path = require('path');
+// var rimraf = require('rimraf');
 
 
-class Image{
-    static init(){
-        this.multer = multer({
-            storage: multer.diskStorage({
-                destination: this.destination.bind(this),
-                filename: this.filename.bind(this)
-            })
-        })
-    }
-    static destination(req, file, cb){
-        mkdirp(this.imageLocation(req.session.user.email), () => 
-            cb(null, this.imageLocation(req.session.user.email))
-        );
+// class Image{
+//     static init(){
+//         this.multer = multer({
+//             storage: multer.diskStorage({
+//                 destination: this.destination.bind(this),
+//                 filename: this.filename.bind(this)
+//             })
+//         })
+//     }
+//     static destination(req, file, cb){
+//         mkdirp(this.imageLocation(req.session.user.email), () => 
+//             cb(null, this.imageLocation(req.session.user.email))
+//         );
         
-    }
-    static filename(req, file, cb) {
-        file.location = this.imagePath(req.session.user.email);
-        file.name = this.imageName(file);
-        this.upload(req, file, (e, user) =>{
-            req.session.user = user;
-            cb(null, file.name);
-        }); 
-    }
+//     }
+//     static filename(req, file, cb) {
+//         file.location = this.imagePath(req.session.user.email);
+//         file.name = this.imageName(file);
+//         this.upload(req, file, (e, user) =>{
+//             req.session.user = user;
+//             cb(null, file.name);
+//         }); 
+//     }
 
 
-    static storage(req, res, cb = function(){}){
-        let single = this.multer.single('image');
-        single(req, res, (err) => {
-            console.log(req.body);
-            if(err)req.error = {error: config.errorMessages.fileUpload}; //::TODO ADD A RETRY
-            if(req.error)User.removeMostRecentImage(req.session.user.email);
-            if(req.body.default)User.setDefaultImage(req.)
-            cb();
-        });
-    }
-    static upload(req, imageObj, cb = function(){}){
-        if(allowedUploads.indexOf(imageObj.mimetype) > -1){
-            User.addImage(req.session.user.email, imageObj, cb);
-        }else{
-            req.error = {error: config.errorMessages.fileType};
-        }
+//     static storage(req, res, cb = function(){}){
+//         let single = this.multer.single('image');
+//         single(req, res, (err) => {
+//             console.log(req.body);
+//             if(err)req.error = {error: config.errorMessages.fileUpload}; //::TODO ADD A RETRY
+//             if(req.error)User.removeMostRecentImage(req.session.user.email);
+//             if(req.body.default)User.setDefaultImage(req.)
+//             cb();
+//         });
+//     }
+//     static upload(req, imageObj, cb = function(){}){
+//         if(allowedUploads.indexOf(imageObj.mimetype) > -1){
+//             User.addImage(req.session.user.email, imageObj, cb);
+//         }else{
+//             req.error = {error: config.errorMessages.fileType};
+//         }
        
-    }
+//     }
 
-    static deleteAllImages(){
-        rimraf('server/images', function () { console.log('done'); });
-        User.clearAllImages();
-    }
+//     static deleteAllImages(){
+//         rimraf('server/images', function () { console.log('done'); });
+//         User.clearAllImages();
+//     }
 
-    static imageName(imageObj){
-        return `profile-image-${Date.now()}.${imageObj.mimetype.replace('image/', '')}`
-    }
-    static imagePath(email){
-        return path.join(email, 'profile');
+//     static imageName(imageObj){
+//         return `profile-image-${Date.now()}.${imageObj.mimetype.replace('image/', '')}`
+//     }
+//     static imagePath(email){
+//         return path.join(email, 'profile');
 
-    }
-    static imageLocation(email){
-        return path.join('server', 'images', email, 'profile');
-    }
+//     }
+//     static imageLocation(email){
+//         return path.join('server', 'images', email, 'profile');
+//     }
 
-}
-Image.init();
-module.exports = Image;
+// }
+// Image.init();
+// module.exports = Image;
