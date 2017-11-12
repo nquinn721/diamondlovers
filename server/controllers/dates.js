bd472e73ebf107013d45202bc440f1ba9e02079f
Nathan <natethepcspecialist@gmail.com>
2017-11-12 10:27:48 -0500

    'update'

--- server/controllers/dates.js
+++ server/controllers/dates.js
@@ -1,19 +1,19 @@
 module.exports = {
 	setDate: function(req, res) {
 		let {to, from, location, time, cost} = req.body;
-		console.log('creating date');
+		console.log('creating date', to, from);
 		
-		Dates.setDate(to, from, location, time, cost, (e, doc) => {
-			Chat.createChat(to, from, function(e, chat) {
-				console.log(chat);
+		// Dates.setDate(to, from, location, time, cost, (e, doc) => {
+		// 	Chat.createChat(to, from, function(e, chat) {
+		// 		console.log(chat);
 				
-				User.createChat(to, from, chat._id, function(e, userDoc) {
-					console.log(userDoc);
+		// 		User.createChat(to, from, chat._id, function(e, userDoc) {
+		// 			console.log(userDoc);
 					
-					res.send(e ? {error: 'failed to create date'} : {data: doc});
-				});
-			});
-		});
+		// 			res.send(e ? {error: 'failed to create date'} : {data: doc});
+		// 		});
+		// 	});
+		// });
 	},
 	getDates: function(req, res) {
 		let _id = req.session.model._id;
