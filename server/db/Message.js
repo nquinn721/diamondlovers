d7d3b4c4a4b550f6cff2b2528903cf34c136e579
Nathan <natethepcspecialist@gmail.com>
2017-11-12 20:30:12 -0500

    'update'

--- server/db/Message.js
+++ server/db/Message.js
@@ -11,7 +11,20 @@ var MessageModel = mongoose.model('Message', MessageSchema);
 
 class Message{
 	static newMessage(chat, owner, message, cb) {
-		MessageModel.create({chat, owner, message}, cb);
+		MessageModel.create({chat, owner, message}, (e, message) => {
+			if(e)return cb(e);
+			MessageModel.findOne(message)
+			.populate({
+				path: 'owner',
+				select: 'profile.displayName profile.defaultImage',
+				model: 'User',
+				populate: {
+					path: 'profile.defaultImage',
+					model: 'Image'
+				}
+			})
+			.exec(cb);	
+		});
 	}
 	static getMessages(chat, cb){
 		MessageModel.find({chat})
