d4898fe6ec8cd3200c3236e7f2ae224309cbf7aa
Nathan <natethepcspecialist@gmail.com>
2017-12-19 10:33:02 -0500

    update

--- server/controllers/yelp.js
+++ server/controllers/yelp.js
@@ -1,6 +1,7 @@
 const Yelp = require('../apis/yelp');
 module.exports = {
 	search: function(req, res) {
+		console.log(req.body);
 		Yelp.search(req.body, function(data) {
 			console.log(data);
 			res.send('hi');
